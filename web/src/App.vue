cbc07c3381ea4fbae5d9369b8a2b84a3e1209407
linzhongxiaosheng <jevonsflash@qq.com>
2022-10-26 16:18:48 +0800

    update web

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,42 +2,212 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <div style="height: 450px">
-      <div v-if="wechatMiniappLoginStatus == 'ACCESSED'">
-        <el-result
-          icon="info"
-          title="已扫码"
-          subTitle="请在小程序上根据提示进行操作"
-        >
-        </el-result>
+    <el-row>
+      <div class="title-container center">
+        <h3 class="title">欢迎登录</h3>
       </div>
-
-      <div v-else-if="wechatMiniappLoginStatus == 'AUTHORIZED'">
-        <el-result
-          icon="success"
-          title="已授权"
-          :subTitle="loading ? '请稍候..' : '正在使用微信账号登录系统'"
-        >
-        </el-result>
-      </div>
-      <div v-else class="center">
-        <img
-          :src="`${prefix}/MiniProgram/GetACode?scene=${getToken()}&page=${miniappPage}&mode=content`"
-        />
-      </div>
-    </div>
-    <div class="center">
-      <el-button
-        v-if="wechatMiniappLoginToken != null"
-        type="primary"
-        size="medium"
-        @click="wechatMiniappLoginToken = null"
-        >刷新</el-button
+    </el-row>
+    <el-row v-if="token == null">
+      <el-tabs
+        tab-position="top"
+        style="display: inline"
+        v-model="activeName"
+        :stretch="true"
       >
-    </div>
-    <div class="center">
+        <el-tab-pane label="账号密码登录" name="account">
+          <el-form
+            ref="loginForm"
+            :model="loginForm"
+            class="login-form"
+            autocomplete="on"
+            label-position="left"
+          >
+            <el-form-item class="item" prop="username">
+              <el-input
+                ref="username"
+                v-model="loginForm.username"
+                :placeholder="'请输入用户名/手机号/邮箱'"
+                name="username"
+                type="text"
+                tabindex="1"
+                autocomplete="on"
+              >
+                <span slot="prepend" class="svg-container">
+                  <i class="el-icon-user" />
+                </span>
+              </el-input>
+            </el-form-item>
+
+            <el-tooltip
+              v-model="capsTooltip"
+              content="大写锁定已打开"
+              placement="right"
+              manual
+            >
+              <el-form-item class="item" prop="password">
+                <el-input
+                  :key="passwordType"
+                  ref="password"
+                  v-model="loginForm.password"
+                  :type="passwordType"
+                  :placeholder="'请输入密码'"
+                  name="password"
+                  tabindex="2"
+                  autocomplete="on"
+                  @keyup.native="checkCapslock"
+                  @blur="capsTooltip = false"
+                  @keyup.enter.native="handleLogin"
+                >
+                  <span slot="prepend" class="svg-container">
+                    <i class="el-icon-lock" />
+                  </span>
+                  <span slot="append" class="show-pwd" @click="showPwd">
+                    <i
+                      :class="
+                        passwordType == 'password'
+                          ? 'el-icon-view'
+                          : 'el-icon-minus'
+                      "
+                    />
+                  </span>
+                </el-input>
+              </el-form-item>
+            </el-tooltip>
+
+            <el-row type="flex" class="row-bg" justify="center">
+              <el-col :span="20">
+                <el-button
+                  :loading="loading"
+                  type="primary"
+                  style="width: 100%"
+                  @click.native.prevent="handleLogin"
+                >
+                  登录
+                </el-button>
+              </el-col>
+            </el-row>        
+          </el-form>
+        </el-tab-pane>
+
+        <el-tab-pane label="手机免密登录" name="phoneNumber">
+          <el-form
+            ref="loginForm"
+            :model="loginForm"
+            class="login-form"
+            autocomplete="on"
+            label-position="left"
+          >
+            <el-form-item class="item" prop="username">
+              <el-input
+                ref="username"
+                v-model="loginForm.username"
+                :placeholder="'请输入手机号'"
+                type="text"
+                tabindex="1"
+                autocomplete="on"
+              >
+                <template slot="prepend">+86</template>
+              </el-input>
+            </el-form-item>
+
+            <el-form-item class="item" prop="password">
+              <el-input
+                ref="password"
+                v-model="loginForm.password"
+                :type="passwordType"
+                :placeholder="'发送验证码后键入验证码'"
+                tabindex="2"
+                autocomplete="on"
+                @blur="capsTooltip = false"
+                @keyup.enter.native="handleLogin"
+              >
+                <el-button
+                  slot="append"
+                  :disabled="smsSendCd > 0"
+                  @click="gotoPhoneNumberVerify('LOGIN')"
+                  >{{
+                    smsSendCd == 0 ? "发送验证码" : smsSendCd + "后重试"
+                  }}</el-button
+                >
+              </el-input>
+            </el-form-item>
+
+            <el-row type="flex" class="row-bg" justify="center">
+              <el-col :span="20">
+                <el-button
+                  :disabled="showVerify == null"
+                  :loading="loading"
+                  type="primary"
+                  style="width: 100%"
+                  @click.native.prevent="handleLogin"
+                >
+                  免密登录
+                </el-button>
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
+    </el-row>
+    <el-row v-else>
+      <el-button
+        :loading="loading"
+        type="danger"
+        style="width: 100%"
+        @click.native.prevent="logout"
+      >
+        退出登录
+      </el-button>
+    </el-row>
+    <el-row class="center" v-if="userInfo != null">
       <span>{{ userInfo }}</span>
-    </div>
+
+      <el-form
+        ref="userInfo"
+        :model="userInfo"
+        autocomplete="on"
+        label-position="left"
+      >
+        <el-form-item :span="12" label="当前手机号" :required="true">
+          <el-input v-model="userInfo.phoneNumber"> </el-input
+        ></el-form-item>
+
+        <el-button
+          v-if="!userInfo.isPhoneNumberConfirmed"
+          size="mini"
+          type="primary"
+          :disabled="smsSendCd > 0"
+          @click="gotoPhoneNumberVerify('BIND_PHONENUMBER')"
+          >{{ smsSendCd == 0 ? "验证手机号" : smsSendCd + "后重试" }}</el-button
+        >
+        <el-button
+          v-else
+          size="mini"
+          type="danger"
+          :disabled="smsSendCd > 0"
+          @click="gotoPhoneNumberVerify('UNBIND_PHONENUMBER')"
+          >{{ smsSendCd == 0 ? "解绑手机号" : smsSendCd + "后重试" }}</el-button
+        >
+
+        <el-form-item
+          v-if="showVerify != null"
+          :span="12"
+          label="验证码"
+          :required="true"
+        >
+          <el-input v-model="verifyNumber">
+            <el-button
+              slot="append"
+              type="primary"
+              size="mini"
+              @click="gotoVerify"
+            >
+              完成验证</el-button
+            >
+          </el-input></el-form-item
+        >
+      </el-form>
+    </el-row>
   </div>
 </template>
 
@@ -47,33 +217,32 @@ import Cookies from "js-cookie";
 
 const tokenKey = "main_token";
 const setToken = (token: string) => Cookies.set(tokenKey, token);
-
+const getToken = () => Cookies.get(tokenKey);
 export default {
   name: "App",
   // components: {HelloWorld},
   data: () => {
     return {
       loading: false,
-      timerId: -1,
-      wechatMiniappLoginToken: null,
-      wechatMiniappLoginStatus: "WAIT",
-      // miniappPage: "",
-      // 小程序未发布时无法跳转至页面，报错41030，https://developers.weixin.qq.com/community/develop/doc/00066c970b4c90f8fd6cbe31e5b400?source=indexmixflow
-      miniappPage: "pages/login/index",
-      prefix: "https://api.matoapp.net:3002/api/services/app",
-      userInfo: "",
-      loginExternalForms: {
-        WeChat: {
-          authProvider: "WeChatAuthProvider",
-          providerKey: "default",
-          providerAccessCode: "",
-        },
+      // host: "https://api.matoapp.net:3002/",
+      host: "http://localhost:44311/",
+      prefix: "api/services/app",
+      userInfo: null,
+      showVerify: null,
+      smsSendCd: 0,
+      activeName: "account",
+      loginForm: {
+        username: "",
+        password: "",
       },
+      passwordType: "password",
+      capsTooltip: false,
+      token: null,
+      verifyNumber: null,
+      timer: null,
     };
   },
-  created: function () {
-    this.start();
-  },
+
   methods: {
     successMessage(value = "执行成功") {
       this.$notify({
@@ -90,113 +259,120 @@ export default {
       });
     },
 
-    getToken() {
-      if (this.wechatMiniappLoginToken == null) {
-        var date = new Date();
-        var token = `${(Math.random() * 100000000)
-          .toFixed(0)
-          .toString()
-          .padEnd(8, "0")}`;
-        this.wechatMiniappLoginToken = token;
-      }
-      return this.wechatMiniappLoginToken;
+    created() {
+      this.token = getToken();
     },
 
-    start() {
-      clearInterval(this.timerId);
-      this.timerId = setInterval(async () => {
-        if (!this.loading) {
-          this.loading = true;
+    showPwd() {
+      if (this.passwordType === "password") {
+        this.passwordType = "";
+      } else {
+        this.passwordType = "password";
+      }
+    },
 
+    checkCapslock(e: KeyboardEvent) {
+      const { key } = e;
+      this.capsTooltip =
+        key != null && key.length == 1 && key >= "A" && key <= "Z";
+    },
+
+    async handleLogin() {
+      this.loading = true;
+
+      let userNameOrEmailAddress = this.loginForm.username;
+      let password = this.loginForm.password;
+      userNameOrEmailAddress = userNameOrEmailAddress.trim();
+      await request(`${this.host}api/TokenAuth/Authenticate`, "post", {
+        userNameOrEmailAddress,
+        password,
+      })
+        .catch((re) => {
+          var res = re.response.data;
+          this.errorMessage(res.error.message);
+        })
+        .then(async (res) => {
+          var data = res.data.result;
+          setToken(data.accessToken);
           await request(
-            `${this.prefix}/MiniProgram/GetToken?token=${this.wechatMiniappLoginToken}`,
+            `${this.host}${this.prefix}/User/GetCurrentUser`,
             "get",
             null
           )
-            .then(async (re) => {
-              if (re.data.result == null) {
-                this.wechatMiniappLoginStatus = "EXPIRED";
-                this.wechatMiniappLoginToken = null;
-                this.loading = false;
-              } else {
-                var result = re.data.result;
-                this.wechatMiniappLoginStatus = result.status;
-                if (
-                  this.wechatMiniappLoginStatus == "AUTHORIZED" &&
-                  result.providerAccessCode != null
-                ) {
-                  await this.handleWxLogin(result.providerAccessCode)
-                    .then(() => {
-                      this.wechatMiniappLoginToken = null;
-                      this.loading = false;
-                    })
-                    .catch((e) => {
-                      this.wechatMiniappLoginToken = null;
-                      this.loading = false;
-                      clearInterval(this.timerId);
-                    });
-                } else {
-                  this.loading = false;
-                }
-              }
+            .catch((re) => {
+              var res = re.response.data;
+              this.errorMessage(res.error.message);
             })
-            .catch((e) => {
-              this.loading = false;
+            .then(async (re) => {
+              var result = re.data.result as any;
+              this.userInfo = result;
+              this.token = getToken();
+              clearInterval(this.timer);
+
+              this.smsSendCd = 0;
+
+              this.successMessage("登录成功");
             });
+        })
+        .finally(() => {
+          setTimeout(() => {
+            this.loading = false;
+          }, 1.5 * 1000);
+        });
+    },
+
+    async gotoPhoneNumberVerify(type) {
+      this.showVerify = type;
+      this.smsSendCd = 60;
+      this.timer = setInterval(() => {
+        this.smsSendCd--;
+        if (this.smsSendCd <= 0) {
+          clearInterval(this.timer);
         }
       }, 1000);
-    },
-
-    async afterLoginSuccess(userinfo) {
-      clearInterval(this.timerId);
-      this.successMessage("登录成功");
-      this.userInfo = userinfo;
-    },
-
-    async externalLogin(userInfo: {
-      authProvider: string;
-      providerKey: string;
-      providerAccessCode: string;
-    }) {
-      let authProvider = userInfo.authProvider;
-      let providerKey = userInfo.providerKey;
-      let providerAccessCode = userInfo.providerAccessCode;
-
-      await request(
-        `https://api.matoapp.net:3002/api/TokenAuth/ExternalAuthenticate`,
-        "post",
-        {
-          authProvider,
-          providerKey,
-          providerAccessCode,
+      await request(`${this.host}${this.prefix}/Captcha/Send`, "post", {
+        phoneNumber: this.loginForm.username,
+        type: type,
+      }).then((re) => {
+        var res = re.data.result;
+        if (res != null && res.bizId == null) {
+          this.errorMessage("发送失败:" + res.message);
+          // this.showVerify = null;
+        } else {
+          this.successMessage("发送验证码成功");
         }
-      ).then(async (res) => {
-        var data = res.data.result;
-        setToken(data.accessToken);
       });
     },
 
-    async handleExternalLogin(authProvider) {
-      // (this.$refs.baseForm as any).validate(async (valid) => {
-      //   if (valid == null) {
-      var currentForms = this.loginExternalForms[authProvider];
+    logout() {
+      setToken(null);
+      this.token = null;
+      this.userInfo = null;
+    },
 
-      this.loading = true;
-      return await this.externalLogin(currentForms).then(async (re) => {
-        return await request(
-          `${this.prefix}/User/GetCurrentUser`,
-          "get",
-          null
-        ).then(async (re) => {
-          var result = re.data.result as any;
-          return await this.afterLoginSuccess(result);
+    async gotoVerify() {
+      var action = null;
+      if (this.showVerify == "BIND_PHONENUMBER") {
+        action = "Bind";
+      } else if (this.showVerify == "UNBIND_PHONENUMBER") {
+        action = "Unbind";
+      } else {
+        action = "Verify";
+      }
+      await request(`${this.host}${this.prefix}/Captcha/${action}`, "post", {
+        token: this.verifyNumber,
+      })
+        .catch((re) => {
+          var res = re.response.data;
+          this.errorMessage(res.error.message);
+        })
+        .then((re) => {
+          var res = re.data;
+          if (res.success) {
+            this.successMessage("绑定成功");
+            this.refreshPage();
+          }
         });
-      });
-    },
-
-    async handleWxLogin(providerAccessCode) {
-      this.loginExternalForms.WeChat.providerAccessCode = providerAccessCode;
-      return await this.handleExternalLogin("WeChat");
     },
   },
 };
